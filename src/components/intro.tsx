f2de475e4887332c41c98bc5a0afd0eaf42e542a
Matthew Alex <matthewalex2005@gmail.com>
2025-04-02 12:11:53 -0400

    lolz

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -7,6 +7,7 @@ interface IntroProps {
 const Intro: React.FC<IntroProps> = ({ names }) => {
     return (
         <div className="p-10">
+            <img src="../assets/game.jpg" alt="gameee" width="1920"/>
             <h1 className="font-bold text-xl pb-4">
                 {names[0]} and {names[1]}
             </h1>
